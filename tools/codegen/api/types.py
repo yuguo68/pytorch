1580cd95c6954ece1fa646126dc843289966c7df
Brian Hirsh <hirsheybar@fb.com>
2021-04-05 15:01:41 -0700

    add C++ namespacing logic to ctypes
    ghstack-source-id: f7219a0c84256f5c330a6fa1246e89cbd34a7f52
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/55047

--- tools/codegen/api/types.py
+++ tools/codegen/api/types.py
@@ -1,6 +1,6 @@
 from tools.codegen.model import *
 from dataclasses import dataclass
-from typing import Optional, Union, Sequence, TypeVar, List, Set
+from typing import Optional, Union, Sequence, TypeVar, List, Set, Dict
 from enum import Enum
 
 _T = TypeVar('_T')
@@ -26,13 +26,70 @@ ArgName = Union[str, SpecialArgName]
 # you need trnsnlations that know about these types, beef up this data
 # structure.
 
+# This class shouldn't be created directly; instead, use/create one of the singletons below.
+@dataclass(frozen=True)
+class BaseCppType:
+    ns: Optional[str]
+    name: str
+
+    def __str__(self) -> str:
+        if self.ns is None or self.ns == '':
+            return self.name
+        return f"{self.ns}::{self.name}"
+
+# The set of all non-templated, valid, fully-qualified names of C++ types that are used in the codegen.
+# Templated types get their own dataclass, mainly to make namespace parsing easier.
+intT = BaseCppType('', 'int64_t')
+doubleT = BaseCppType('', 'double')
+boolT = BaseCppType('', 'bool')
+voidT = BaseCppType('', 'void')
+stringT = BaseCppType('std', 'string')
+generatorT = BaseCppType('at', 'Generator')
+scalarTypeT = BaseCppType('at', 'ScalarType')
+tensorT = BaseCppType('at', 'Tensor')
+tensorListT = BaseCppType('at', 'TensorList')
+dimnameT = BaseCppType('at', 'Dimname')
+dimnameListT = BaseCppType('at', 'DimnameList')
+layoutT = BaseCppType('at', 'Layout')
+deviceT = BaseCppType('at', 'Device')
+scalarT = BaseCppType('at', 'Scalar')
+memoryFormatT = BaseCppType('at', 'MemoryFormat')
+qschemeT = BaseCppType('at', 'QScheme')
+storageT = BaseCppType('at', 'Storage')
+streamT = BaseCppType('at', 'Stream')
+intArrayRefT = BaseCppType('at', 'IntArrayRef')
+tensorOptionsT = BaseCppType('at', 'TensorOptions')
+
+BaseTypeToCppMapping: Dict[BaseTy, BaseCppType] = {
+    BaseTy.int: intT,
+    BaseTy.float: doubleT,
+    BaseTy.bool: boolT,
+    BaseTy.str: stringT,
+    BaseTy.Generator: generatorT,
+    BaseTy.ScalarType: scalarTypeT,
+    BaseTy.Tensor: tensorT,
+    BaseTy.Dimname: dimnameT,
+    BaseTy.Layout: layoutT,
+    BaseTy.Device: deviceT,
+    BaseTy.Scalar: scalarT,
+    BaseTy.MemoryFormat: memoryFormatT,
+    BaseTy.QScheme: qschemeT,
+    BaseTy.Storage: storageT,
+    BaseTy.Stream: streamT,
+}
+
 @dataclass(frozen=True)
 class BaseCType:
-    type: str
+    type: BaseCppType
     name: ArgName
 
     def cpp_type(self, *, strip_ref: bool = False) -> str:
-        return self.type
+        return str(self.type)
+
+    # For BC reasons, we don't want to introduce at:: namespaces to RegistrationDeclarations.yaml
+    # TODO: Kill this when we eventually remove it!
+    def cpp_type_registration_declarations(self) -> str:
+        return str(self.type).replace('at::', '')
 
 @dataclass(frozen=True)
 class ConstRefCType:
@@ -42,6 +99,9 @@ class ConstRefCType:
         if strip_ref:
             return self.elem.cpp_type(strip_ref=strip_ref)
         return f'const {self.elem.cpp_type()} &'
+
+    def cpp_type_registration_declarations(self) -> str:
+        return f'const {self.elem.cpp_type_registration_declarations()} &'
 
     @property
     def name(self) -> ArgName:
@@ -56,6 +116,9 @@ class MutRefCType:
             return self.elem.cpp_type(strip_ref=strip_ref)
         return f'{self.elem.cpp_type()} &'
 
+    def cpp_type_registration_declarations(self) -> str:
+        return f'{self.elem.cpp_type_registration_declarations()} &'
+
     @property
     def name(self) -> ArgName:
         return self.elem.name
@@ -67,6 +130,9 @@ class OptionalCType:
     def cpp_type(self, *, strip_ref: bool = False) -> str:
         # Do not pass `strip_ref` recursively.
         return f'c10::optional<{self.elem.cpp_type()}>'
+
+    def cpp_type_registration_declarations(self) -> str:
+        return f'c10::optional<{self.elem.cpp_type_registration_declarations()}>'
 
     @property
     def name(self) -> ArgName:
@@ -80,6 +146,9 @@ class ListCType:
         # Do not pass `strip_ref` recursively.
         return f'c10::List<{self.elem.cpp_type()}>'
 
+    def cpp_type_registration_declarations(self) -> str:
+        return f'c10::List<{self.elem.cpp_type_registration_declarations()}>'
+
     @property
     def name(self) -> ArgName:
         return self.elem.name
@@ -90,7 +159,10 @@ class ArrayRefCType:
 
     def cpp_type(self, *, strip_ref: bool = False) -> str:
         # Do not pass `strip_ref` recursively.
-        return f'ArrayRef<{self.elem.cpp_type()}>'
+        return f'at::ArrayRef<{self.elem.cpp_type()}>'
+
+    def cpp_type_registration_declarations(self) -> str:
+        return f'ArrayRef<{self.elem.cpp_type_registration_declarations()}>'
 
     @property
     def name(self) -> ArgName:
@@ -103,6 +175,9 @@ class VectorCType:
     def cpp_type(self, *, strip_ref: bool = False) -> str:
         # Do not pass `strip_ref` recursively.
         return f'std::vector<{self.elem.cpp_type()}>'
+
+    def cpp_type_registration_declarations(self) -> str:
+        return f'std::vector<{self.elem.cpp_type_registration_declarations()}>'
 
     @property
     def name(self) -> ArgName:
@@ -117,6 +192,9 @@ class ArrayCType:
         # Do not pass `strip_ref` recursively.
         return f'std::array<{self.elem.cpp_type()},{self.size}>'
 
+    def cpp_type_registration_declarations(self) -> str:
+        return f'std::array<{self.elem.cpp_type_registration_declarations()}, {self.size}>'
+
     @property
     def name(self) -> ArgName:
         return self.elem.name
@@ -129,12 +207,25 @@ class TupleCType:
         # Do not pass `strip_ref` recursively.
         return f'std::tuple<{",".join([e.cpp_type() for e in self.elems])}>'
 
+    def cpp_type_registration_declarations(self) -> str:
+        return f'std::tuple<{",".join([e.cpp_type_registration_declarations() for e in self.elems])}>'
+
     @property
     def name(self) -> ArgName:
         # N.B. this isn't currently used anywhere: std::tuple is only used as a return type, which doesn't use names.
         raise AssertionError("std::tuple isn't currently used as an argument anywhere, and doesn't require a name.")
 
-CType = Union[BaseCType, OptionalCType, ConstRefCType, MutRefCType, ListCType, ArrayRefCType, ArrayCType, VectorCType, TupleCType]
+CType = Union[
+    BaseCType,
+    OptionalCType,
+    ConstRefCType,
+    MutRefCType,
+    ListCType,
+    ArrayRefCType,
+    ArrayCType,
+    VectorCType,
+    TupleCType
+]
 
 # A binding represents any C++ binding site for a formal parameter.
 # We don't distinguish between binding sites for different APIs;
@@ -167,6 +258,15 @@ class Binding:
         if self.default is not None:
             mb_default = f"={self.default}"
         return f"{self.type} {self.name}{mb_default}"
+
+    # For BC reasons, we don't want to introduce at:: namespaces to RegistrationDeclarations.yaml
+    # TODO: Kill this when we eventually remove it!
+    def decl_registration_declarations(self) -> str:
+        type_s = self.ctype.cpp_type_registration_declarations()
+        mb_default = ""
+        if self.default is not None:
+            mb_default = f"={self.default}"
+        return f"{type_s} {self.name}{mb_default}"
 
     def defn(self) -> str:
         return f"{self.type} {self.name}"
--- tools/codegen/dest/register_dispatch_key.py
+++ tools/codegen/dest/register_dispatch_key.py
@@ -457,14 +457,14 @@ return {sig.name()}({', '.join(e.expr for e in translate(cpp_sig.arguments(), si
             # After running meta, op.outputs_ is guaranteed to be valid;
             # add it to the context
             # TODO: handle multi-return
-            assert ConstRefCType(BaseCType("Tensor", structured.out_arguments(self.g)[0].ctype.name)) == \
+            assert ConstRefCType(BaseCType(tensorT, structured.out_arguments(self.g)[0].ctype.name)) == \
                 structured.out_arguments(self.g)[0].ctype
             context.append(Expr(
                 expr="op.outputs_[0]",
                 # TODO: Stop hardcoding that the output type is a Tensor.  Note
                 # that for the codegen here this is fine because outputs_ is
                 # hardcoded to be tensor already
-                type=MutRefCType(BaseCType("Tensor", structured.out_arguments(self.g)[0].ctype.name)),
+                type=MutRefCType(BaseCType(tensorT, structured.out_arguments(self.g)[0].ctype.name)),
             ))
 
             # With the expanded context, do the impl call (if not a meta
